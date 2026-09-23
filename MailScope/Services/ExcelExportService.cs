0b062f223740ad8966437a443944b904e7450f2c
Xugg <xuguangge@sgrs-semi.com>
2026-09-23 11:01:17 +0800

    Fix export excele issue.

--- MailScope/Services/ExcelExportService.cs
+++ MailScope/Services/ExcelExportService.cs
@@ -12,75 +12,43 @@ public class ExcelExportService
         IEnumerable<EmailContact> contacts
     )
     {
-        ExcelPackage.LicenseContext =
-            LicenseContext.NonCommercial;
-        using var package =
-            new ExcelPackage();
-        CreateContactSheet(
-            package,
-            contacts
-        );
-        CreateDomainSheet(
-            package,
-            contacts
-        );
-        package.SaveAs(
-            new FileInfo(filePath)
-        );
+        ExcelPackage.LicenseContext =LicenseContext.NonCommercial;
+        using var package =new ExcelPackage();
+        CreateContactSheet(package,contacts);
+        CreateDomainSheet(package,contacts);
+        package.SaveAs(new FileInfo(filePath));
     }
-    private void CreateContactSheet
-    (
-        ExcelPackage package,
-        IEnumerable<EmailContact> contacts
-    )
+    private void CreateContactSheet( ExcelPackage package, IEnumerable<EmailContact> contacts )
     {
-        var sheet =
-            package.Workbook.Worksheets.Add(
-                "联系人"
-            );
-        sheet.Cells[1, 1].Value =
-            "邮箱";
-        sheet.Cells[1, 2].Value =
-            "次数";
-        sheet.Cells[1, 3].Value =
-            "最近联系";
-        sheet.Cells[1, 4].Value =
-            "域名";
-        sheet.Cells[1, 5].Value =
-            "邮箱类型";
+        var sheet =package.Workbook.Worksheets.Add("联系人");
+        sheet.Cells[1, 1].Value = "邮箱";
+        sheet.Cells[1, 2].Value = "次数";
+        sheet.Cells[1, 3].Value = "最近联系";
+        sheet.Cells[1, 4].Value = "域名";
+        sheet.Cells[1, 5].Value = "邮箱类型";
         int row = 2;
+
         foreach (var item in contacts)
         {
-            sheet.Cells[row, 1].Value =
-                item.Email;
-            sheet.Cells[row, 2].Value =
-                item.Count;
-            sheet.Cells[row, 3].Value =
-                item.LastTime;
-            sheet.Cells[row, 4].Value =
-                item.Domain;
-            sheet.Cells[row, 5].Value =
-                item.ProviderType;
+            sheet.Cells[row, 1].Value =item.Email;
+            sheet.Cells[row, 2].Value =item.Count;
+            // 最近联系时间
+            sheet.Cells[row, 3].Value =item.LastTime;
+            sheet.Cells[row, 3].Style.Numberformat.Format ="yyyy-mm-dd hh:mm:ss";
+            sheet.Cells[row, 4].Value =item.Domain;
+            sheet.Cells[row, 5].Value =item.ProviderType;
             row++;
         }
+
         FormatSheet(sheet);
     }
-    private void CreateDomainSheet
-    (
-        ExcelPackage package,
-        IEnumerable<EmailContact> contacts
-    )
+   
+    private void CreateDomainSheet (ExcelPackage package,IEnumerable<EmailContact> contacts )
     {
-        var sheet =
-            package.Workbook.Worksheets.Add(
-                "域名统计"
-            );
-        sheet.Cells[1, 1].Value =
-            "域名";
-        sheet.Cells[1, 2].Value =
-            "数量";
-        var groups =
-            contacts
+        var sheet =package.Workbook.Worksheets.Add("域名统计");
+        sheet.Cells[1, 1].Value ="域名";
+        sheet.Cells[1, 2].Value ="数量";
+        var groups =contacts
             .GroupBy(x => x.Domain)
             .Select(x => new
             {
@@ -101,9 +69,7 @@ public class ExcelExportService
         }
         FormatSheet(sheet);
     }
-    private void FormatSheet(
-        ExcelWorksheet sheet
-    )
+    private void FormatSheet(ExcelWorksheet sheet)
     {
         sheet.Cells.AutoFitColumns();
         using var range =
